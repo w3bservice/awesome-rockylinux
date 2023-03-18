a1883c4669afdf2232a0ab14db63b21aef1f1fdf
pierls <80945072+pierls@users.noreply.github.com>
2023-03-18 16:57:53 +0100

    First draft of a running script

--- awesome fedora config.sh
+++ awesome fedora config.sh
@@ -1,3 +1,6 @@
+# Ensure that if an error occur somewhere, the sript stops
+set -o errexit
+
 # Awesome install part
 sudo dnf install awesome.x86_64
 
@@ -5,7 +8,11 @@ sudo dnf install awesome.x86_64
 sudo dnf install vicious.noarch
 
 echo "[i] Creating /usr/share/applications/awesome.desktop ..."
-touch /usr/share/applications/awesome.desktop
+sudo touch /usr/share/applications/awesome.desktop
+
+#idk why, but this file needs to be 777
+sudo chmod 777 /usr/share/applications/awesome.desktop
+
 echo "[Desktop Entry]"                          > /usr/share/applications/awesome.desktop
 echo "Version=1.0"                              >> /usr/share/applications/awesome.desktop
 echo "Type=Application"                         >> /usr/share/applications/awesome.desktop
@@ -14,13 +21,22 @@ echo "Comment=The awesome launcher!"    		>> /usr/share/applications/awesome.des
 echo "TryExec=awesome"                          >> /usr/share/applications/awesome.desktop
 echo "Exec=awesome"                             >> /usr/share/applications/awesome.desktop
 
+sudo chmod 711 /usr/share/applications/awesome.desktop
+
 echo "[+] Succsessfully created the application launcher!"
 echo "[i] Creating /usr/share/gnome-session/sessions/awesome.session ..."
-touch /usr/share/gnome-session/sessions/awesome.session
+sudo touch /usr/share/gnome-session/sessions/awesome.session
+
+#idem here
+sudo chmod 777 /usr/share/gnome-session/sessions/awesome.session
+
 echo "[GNOME Session]"                                                  > /usr/share/gnome-session/sessions/awesome.session
 echo "Name=Awesome session"                                             >> /usr/share/gnome-session/sessions/awesome.session
 echo "RequiredComponents=awesome;gnome-settings-daemon;"        		>> /usr/share/gnome-session/sessions/awesome.session
 echo "DesktopName=Awesome"                                              >> /usr/share/gnome-session/sessions/awesome.session
+
+#back to normal now
+sudo chmod 711 /usr/share/gnome-session/sessions/awesome.session
 
 echo "[+] Succsessfully created session file!"
 echo "[i] Cloning theme from git..."
@@ -31,8 +47,10 @@ echo "[+] git cloned!"
 echo "[i] moving config files..."
 
 #my personal choice on these themes, can be just changed and noramlly should work
-THEME_CHOSEN = 7
+THEME_CHOSEN=7
 
+rm -rf ~/.config/awesome
+mkdir ~/.config/awesome
 mv -bv awesome-copycats/* ~/.config/awesome; rm -rf awesome-copycats
 
 cp ~/.config/awesome/rc.lua.template ~/.config/awesome/rc.lua
@@ -47,13 +65,15 @@ echo "[+] files moved!"
 
 #This part is to ensure that the addons required by the differents themes will be really installed
 echo "[i] Cloning freedesktop addon..."
-
+rm -rf ~/.config/awesome/freedesktop
 mkdir ~/.config/awesome/freedesktop
 git clone https://github.com/lcpz/awesome-freedesktop.git ~/.config/awesome/freedesktop
 
 echo "[+] Done !"
 echo "[i] Cloning lain addon..."
 
+
+rm -rf ~/.config/awesome/lain
 mkdir ~/.config/awesome/lain
 git clone https://github.com/lcpz/lain.git ~/.config/awesome/lain
 
@@ -63,4 +83,4 @@ echo "[+] Done!"
 #Misc setup
 
 #To have FR/EN keyboard swich using alt+shift
-setxkbmap -layout "us,gr" -option "grp:alt_shift_toggle"
+setxkbmap -layout "us,fr" -option "grp:alt_shift_toggle"
