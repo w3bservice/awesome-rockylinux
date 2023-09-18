164c952f4b4706dd4c6eada35b15036ccae82699
plouis <plouis@2a01cb0130763fb2b1ca2d7110c4b2b5.ipv6.abo.wanadoo.fr>
2023-09-18 15:46:51 -0400

    updated nvim boilerplate install script

--- neovim_config.sh
+++ neovim_config.sh
@@ -1,9 +1,33 @@
-# Installing neovim boilerplate (https://github.com/tokiory/neovim-boilerplate)
-[ -d $HOME/.config/nvim ] && mv $HOME/.config/{nvim,nvim.old}; \
-    git clone https://github.com/tokiory/neovim-boilerplate $HOME/.config/nvim
+# This script only aims to have a clean installation of 
+# Neovim boilerplate on a fresh Fedora, with all its prerequistres.
+
+
+# Installing gcc (cause Fedoras doesent have it)
+sudo yum -y install gcc
+
+# And gcc for cpp cuz why not
+yum install gcc-c++
 
 # Installing neovim via AppImage
+#### Deperecated version ####
+# curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+# chmod u+x nvim.appimage
+# ./nvim.appimage
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage
+# Installing python prerequistres
+sudo dnf install -y python3-pip
+sudo dnf install -y neovim python3-neovim
+
+# NodeJS time
+sudo dnf install -y nodejs
+
+# Finally: golang
+sudo dnf install -y go
+
+
+# Installing neovim boilerplate (https://github.com/tokiory/neovim-boilerplate)
+[ -d $HOME/.config/nvim ] && mv -vn $HOME/.config/{nvim,nvim.old}; \
+    git clone https://github.com/tokiory/neovim-boilerplate $HOME/.config/nvim
+
+# Letting Neovim installing its own dependencies:
+nvim
